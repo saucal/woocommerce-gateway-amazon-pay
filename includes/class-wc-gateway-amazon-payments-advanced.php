d02bfa486145fa3d8b3cda093f1f8b9b9eb84d7b
Laura <lauramelos@gmail.com>
2021-11-12 17:25:31 -0300

    Hide API V1 after onboarding is made.

--- includes/class-wc-gateway-amazon-payments-advanced.php
+++ includes/class-wc-gateway-amazon-payments-advanced.php
@@ -2305,7 +2305,7 @@ class WC_Gateway_Amazon_Payments_Advanced extends WC_Gateway_Amazon_Payments_Adv
 			return;
 		}
 
-		if ( 'yes' !== $this->settings['hide_standard_checkout_button'] || $this->has_other_gateways_enabled() ) {
+		if ( 'yes' !== $this->settings['hide_standard_checkout_button'] ) {
 			return;
 		}
 
